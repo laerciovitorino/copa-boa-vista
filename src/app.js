de59dc5d41b1d2e3641578ad766cd5264c52329d
Laércio Vitorino <laerciovitorino@gmail.com>
2019-05-27 11:16:22 -0300

    Add webpack to the project and update both package.json and app.js file

--- src/app.js
+++ src/app.js
@@ -1,55 +1,57 @@
-class Main extends React.Component {
-  render() {
-    return (
-      <div>
-        Main Component
-        <Header />
-        <Table />
-        <Games />
-        <TopScorer />
-      </div>
-    );
-  }
-}
+console.log('copa boa vista is running!');
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        Header Component
-      </div>
-    );
-  }
-}
+// class Main extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         Main Component
+//         <Header />
+//         <Table />
+//         <Games />
+//         <TopScorer />
+//       </div>
+//     );
+//   }
+// }
 
-class Table extends React.Component {
-  render() {
-    return (
-      <div>
-        Table Component
-      </div>
-    );
-  }
-}
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         Header Component
+//       </div>
+//     );
+//   }
+// }
 
-class Games extends React.Component {
-  render() {
-    return (
-      <div>
-        Games Component
-      </div>
-    );
-  }
-}
+// class Table extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         Table Component
+//       </div>
+//     );
+//   }
+// }
 
-class TopScorer extends React.Component {
-  render() {
-    return (
-      <div>
-        Top Scorer
-      </div>
-    );
-  }
-}
+// class Games extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         Games Component
+//       </div>
+//     );
+//   }
+// }
 
-ReactDOM.render(<Main />, document.getElementById('app'));
+// class TopScorer extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         Top Scorer
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<Main />, document.getElementById('app'));
