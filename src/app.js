858268c0a46e695407a0142b89ff9f7b83ccf82f
Laércio Vitorino <laerciovitorino@gmail.com>
2019-05-28 16:28:42 -0300

    Install some modules: react, react-dom, validator, babel-core and babel-loader, edit webpack.config.js and create .babelrc file; Steps necessary to write jsx code and get it rendered to the screen

--- src/app.js
+++ src/app.js
@@ -1,57 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 console.log('copa boa vista is running!');
 
-// class Main extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         Main Component
-//         <Header />
-//         <Table />
-//         <Games />
-//         <TopScorer />
-//       </div>
-//     );
-//   }
-// }
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        Main Component
+        <Header />
+        <Table />
+        <Games />
+        <TopScorer />
+      </div>
+    );
+  }
+}
 
-// class Header extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         Header Component
-//       </div>
-//     );
-//   }
-// }
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        Header Component
+      </div>
+    );
+  }
+}
 
-// class Table extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         Table Component
-//       </div>
-//     );
-//   }
-// }
+class Table extends React.Component {
+  render() {
+    return (
+      <div>
+        Table Component
+      </div>
+    );
+  }
+}
 
-// class Games extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         Games Component
-//       </div>
-//     );
-//   }
-// }
+class Games extends React.Component {
+  render() {
+    return (
+      <div>
+        Games Component
+      </div>
+    );
+  }
+}
 
-// class TopScorer extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         Top Scorer
-//       </div>
-//     );
-//   }
-// }
+class TopScorer extends React.Component {
+  render() {
+    return (
+      <div>
+        Top Scorer
+      </div>
+    );
+  }
+}
 
-// ReactDOM.render(<Main />, document.getElementById('app'));
+ReactDOM.render(<Main />, document.getElementById('app'));
