2697e037a6cc70bb50109513282e79150be479e5
Laércio Vitorino <laerciovitorino@gmail.com>
2019-06-07 11:14:41 -0300

    Add some loader(css, style, sass) and normalize.css in order to enhance and make it easy the styling process

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
 import Tournament from './components/Tournament';
 
