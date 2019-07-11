ea508ebf1a892577fce553587043287b9121ba9a
Laércio Vitorino <laerciovitorino@gmail.com>
2019-07-11 11:27:44 -0300

    Improve the top scorers section look and feel

--- src/components/TopScorer.js
+++ src/components/TopScorer.js
@@ -1,29 +1,30 @@
 import React from 'react';
 
+import * as Scorers from '../resources/data/topScorers.json';
+
 export default class TopScorer extends React.Component {
+  // fazer a função que irá calcular as posições dos artilheiros
   render() {
     return (
       <div>
         <h2 className="scorers__title">Artilheiros</h2>
+        <div className="scorers__subtitle d-flex justify-content-between">
+          <span className="scorers__subtitle__player">Classificação</span>
+          <span className="scorers__subtitle__goals">Gols</span>
+        </div>
         <ul className="list-group list-group-flush scorers">
-          <li className="scorers__subtitle d-flex justify-content-between">
-            <span className="scorers__subtitle__player">Jogador</span>
-            <span className="scorers__subtitle__goals">Gols</span>
-          </li>
-          <li className="list-group-item">
-            <span># </span>
-            <span>foto </span>
-            <span>símbolo </span>
-            <span>Nome </span>
-            <span>#</span>
-          </li>
-          <li className="list-group-item">
-            <span># </span>
-            <span>foto </span>
-            <span>símbolo </span>
-            <span>Nome </span>
-            <span>#</span>
-          </li>
+          {Object.values(Scorers)[0].map((scorer) => (
+            <li className="list-group-item" key={scorer}>
+              <span>{1}</span>
+              <span><img src={require('../resources/images/logos/' + scorer["photo"])} className="scorers__photo" /></span>
+              <span><img src={require('../resources/images/logos/' + scorer["shield"])} className="scorers__shield" /></span>
+              <div className="scorers__name_position">
+                <div className="scorers__name">{scorer["name"]}</div>
+                <div className="scorers__position">{scorer["position"]}</div>
+              </div>
+              <span className="scorers_goals">{scorer["goals"]}</span>
+            </li>
+          ))}
         </ul>
       </div>
     );
